d76a308b544328958d4fa42b4dc03c9636be961b
Neven Kajić <nkajic@users.noreply.github.com>
2020-11-18 16:06:15 +0100

    WIP

--- src/view/frontend/web/js/mixin/email-mixin.js
+++ src/view/frontend/web/js/mixin/email-mixin.js
@@ -2,17 +2,61 @@
  * Copyright © 2018 Rubic. All rights reserved.
  * See LICENSE.txt for license details.
  */
-define([], function () {
+define([
+    'jquery'
+], function (
+    $
+) {
     'use strict';
 
     return function (target) {
         return target.extend({
+            formNextStepSelector: '#checkoutSteps form.form-next-step',
+
             /**
-             * Reduce form delay when checking if entered email already exists.
+             * Initializes observable properties of instance
+             *
+             * @returns {Object} Chainable.
              */
-            initialize: function () {
-                this.checkDelay = 500;
-                return this._super();
+            initObservable: function () {
+                this._super()
+                    .observe([
+                        'isRegisterButtonVisible',
+                        'isLoginButtonVisible'
+                    ]);
+
+                this.email.subscribe(this.emailAddressChanged, this);
+                this.isPasswordVisible.subscribe(this.haveAccountChanged, this);
+
+                var haveAccount = Boolean(this.resolveInitialPasswordVisibility());
+                this.isRegisterButtonVisible(!haveAccount);
+                this.isLoginButtonVisible(haveAccount);
+                this.displayFormNavigateToNextStep(!haveAccount);
+
+                return this;
+            },
+
+            emailAddressChanged: function (email) {
+                this.isRegisterButtonVisible(this.validateEmail());
+                this.displayFormNavigateToNextStep(this.validateEmail());
+            },
+
+            haveAccountChanged: function (haveAccount) {
+                haveAccount = Boolean(haveAccount);
+                if (haveAccount) {
+                    this.isRegisterButtonVisible(false);
+                    this.displayFormNavigateToNextStep(false);
+                }
+                this.isLoginButtonVisible(haveAccount);
+            },
+
+            displayFormNavigateToNextStep: function (value) {
+                var formNextStep = $(this.formNextStepSelector);
+                if (value) {
+                    formNextStep.show();
+                } else {
+                    formNextStep.hide();
+                }
             }
         });
     }
