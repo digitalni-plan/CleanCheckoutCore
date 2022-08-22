1a72c325f42a82177d49205933563c2d65885146
Neven Kajić <nkajic@users.noreply.github.com>
2022-08-22 09:17:19 +0200

    Test

--- src/Observer/SetDefaultShippingObserver.php
+++ src/Observer/SetDefaultShippingObserver.php
@@ -9,6 +9,7 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Directory\Helper\Data as DirectoryHelper;
+use Magento\Quote\Model\ShippingMethodManagement;
 use Magento\Quote\Model\Quote;
 use Magento\Store\Model\ScopeInterface;
 use Rubic\CleanCheckout\ConfigProvider\CheckoutConfigProvider;
@@ -17,24 +18,18 @@ class SetDefaultShippingObserver implements ObserverInterface
 {
     const CONFIG_PATH_DEFAULT_SHIPPING_METHOD = 'clean_checkout/general/default_shipping_method';
 
-    /**
-     * @var ScopeConfigInterface
-     */
-    private $scopeConfig;
+    private ScopeConfigInterface $scopeConfig;
+    private DirectoryHelper $directoryHelper;
+    private ShippingMethodManagement $shippingMethodManagement;
 
-    /**
-     * @var DirectoryHelper
-     */
-    private $directoryHelper;
-
-    /**
-     * @param ScopeConfigInterface $scopeConfig
-     * @param DirectoryHelper $directoryHelper
-     */
-    public function __construct(ScopeConfigInterface $scopeConfig, DirectoryHelper $directoryHelper)
-    {
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        DirectoryHelper $directoryHelper,
+        ShippingMethodManagement $shippingMethodManagement
+    ) {
         $this->scopeConfig = $scopeConfig;
         $this->directoryHelper = $directoryHelper;
+        $this->shippingMethodManagement = $shippingMethodManagement;
     }
 
     /**
@@ -61,6 +56,15 @@ class SetDefaultShippingObserver implements ObserverInterface
     }
 
     /**
+     * @param Quote $quote
+     * @return \Magento\Quote\Api\Data\ShippingMethodInterface[]
+     */
+    private function getEstimateShippingMethods(Quote $quote)
+    {
+         return $this->shippingMethodManagement->estimateByExtendedAddress($quote->getId(), $quote->getShippingAddress());
+    }
+
+    /**
      * @param Observer $observer
      * @return void
      */
@@ -78,8 +82,17 @@ class SetDefaultShippingObserver implements ObserverInterface
             if (!$shippingAddress->getCountryId()) {
                 $shippingAddress->setCountryId($this->directoryHelper->getDefaultCountry());
             }
-            $shippingAddress->setCollectShippingRates(true)
-                ->setShippingMethod($this->getDefaultShippingMethod());
+
+            foreach ($shippingAddress->getAllShippingRates() as $shippingRate) {
+                $shippingRateCode = $shippingRate->getCarrier() . '_' . $shippingRate->getMethod();
+
+                $olala = substr($shippingRateCode, 0, strlen($this->getDefaultShippingMethod()));
+
+                if ($olala == $this->getDefaultShippingMethod()) {
+                    $shippingAddress->setCollectShippingRates(true)
+                        ->setShippingMethod($shippingRateCode);
+                }
+            }
         }
     }
 }
